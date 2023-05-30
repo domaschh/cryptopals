8166c64b43485ccb14edf0616d156e21ce77bb54
Domasch <magesthomas@gmail.com>
2023-05-30 18:42:38 +0200

    refactor

--- src/challengetwo/two.rs
+++ src/challengetwo/two.rs
@@ -1,83 +1,106 @@
-use aes::cipher::{generic_array::GenericArray, BlockEncrypt, KeyInit};
+use aes::{
+    cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit},
+    Aes128,
+};
 
-pub fn pad_to_length(src: &[u8], len: usize) -> Vec<u8> {
-    if src.len() >= len {
-        return src[0..16].into();
+use crate::utils::utils;
+
+// [1,2,3], 2 -> [1,2,3,1]
+// [1,2,3,4] 3 -> [1,2,3,4,2,2]
+// [1,2,3,4,5,6,7] -> [1,2,3,4,5,6,7,2,2]
+pub fn pad_to_length(src: &[u8], block_len: usize) -> Vec<u8> {
+    if block_len == 1 || block_len == src.len() {
+        src.to_vec()
     } else {
-        //can't use vec::with_capacity here because of min size and size differnence
-        //https://stackoverflow.com/questions/63426583/performance-penalty-of-using-clone-from-slice-instead-of-copy-from-slice#:~:text=In%20Rust%2C%20there%20are%20two,the%20type%20to%20implement%20Copy%20.
-        let mut new_slice = vec![0; len];
-        new_slice[0..src.len()].copy_from_slice(src);
-        new_slice[src.len()..len].fill(len as u8 - src.len() as u8);
-        return new_slice;
+        let padding_length = block_len - (src.len() % block_len);
+        let mut padded_data = src.to_vec();
+        padded_data.extend(vec![padding_length as u8; padding_length]);
+        padded_data
     }
 }
 
-pub fn encrypt_aes_block_str(block: &str, key_str: &str, last: &str) -> Vec<u8> {
-    let padded_block = pad_to_length(block.as_bytes(), 16);
-    let padded_last = pad_to_length(last.as_bytes(), 16);
-    let mut xored: Vec<_> = padded_last
-        .iter()
-        .zip(padded_block.iter())
-        .map(|(b1, b2)| b1 ^ b2)
-        .collect();
+pub fn encrypt_aes_cbc(message: &str, key_str: &str, iv_str: u8, block_size: usize) -> String {
+    let msg_bytes = pad_to_length(message.as_bytes(), block_size);
+    let iv = std::iter::repeat(iv_str).take(block_size).collect();
 
     let key = GenericArray::clone_from_slice(key_str.as_bytes());
-    let mut block = GenericArray::clone_from_slice(&xored);
-    let cipher = aes::Aes128::new(&key);
-    cipher.encrypt_block(&mut block);
+    let cipher = Aes128::new(&key);
+    let mut encrypted_blocks: Vec<Vec<u8>> = Vec::new();
+    (0..message.len()).step_by(16).for_each(|x| {
+        // Take last encrypted block or IV for first block iteration
+        let last = encrypted_blocks.last().unwrap_or(&iv);
 
-    block.into_iter().collect::<Vec<_>>()
+        // XOR last encrypted block with current msg block & encrypt result
+        let xor_block = utils::xor_bytes(last, &msg_bytes[x..x + 16]);
+        let mut block = GenericArray::clone_from_slice(&xor_block);
+        cipher.encrypt_block(&mut block);
+
+        encrypted_blocks.push(block.into_iter().collect::<Vec<u8>>());
+    });
+
+    hex::encode(encrypted_blocks.into_iter().flatten().collect::<Vec<u8>>())
 }
 
-pub fn encrypt_aes_block(block: &[u8], key_str: &[u8], last: &[u8]) -> Vec<u8> {
-    let padded_block = pad_to_length(block, 16);
-    let padded_last = pad_to_length(last, 16);
-    let mut xored: Vec<_> = padded_last
-        .iter()
-        .zip(padded_block.iter())
-        .map(|(b1, b2)| b1 ^ b2)
-        .collect();
+pub fn decrypt_aes_cbc(cipher_hex: &str, key_str: &str, iv_str: u8, block_size: usize) -> String {
+    let encrypted_bytes = hex::decode(cipher_hex).unwrap();
+    let key = GenericArray::clone_from_slice(key_str.as_bytes());
+    let iv: Vec<u8> = std::iter::repeat(iv_str).take(block_size).collect();
 
-    let key = GenericArray::clone_from_slice(key_str);
-    let mut block = GenericArray::clone_from_slice(&xored);
-    let cipher = aes::Aes128::new(&key);
-    cipher.encrypt_block(&mut block);
+    let cipher = Aes128::new(&key);
 
-    block.into_iter().collect::<Vec<_>>()
+    let mut decrypted_blocks: Vec<Vec<u8>> = Vec::new();
+    (0..encrypted_bytes.len()).step_by(16).for_each(|x| {
+        // Take last of encrypted block or IV in case of first block iteration
+        let last = if x == 0 {
+            &iv
+        } else {
+            &encrypted_bytes[x - 16..x]
+        };
+
+        // Decrypt AES
+        let mut block = GenericArray::clone_from_slice(&encrypted_bytes[x..x + 16]);
+        cipher.decrypt_block(&mut block);
+        let decrypted_block = block.into_iter().collect::<Vec<u8>>();
+
+        // XOR decrypted block with last encrypted block to undo xor during encryption
+        let xor_block = utils::xor_bytes(last, &decrypted_block);
+        decrypted_blocks.push(xor_block);
+    });
+
+    // Get number of padding bytes applied during encryption & remove padding
+    let padding_byte = *decrypted_blocks.last().unwrap().last().unwrap() as usize;
+    decrypted_blocks
+        .into_iter()
+        .flatten()
+        .take(encrypted_bytes.len() - padding_byte)
+        .map(|x| x as char)
+        .collect::<String>()
 }
 
-pub fn encrypt_with_aes(text: &str, iv_key: u8, key: &str) -> Vec<u8> {
-    let mut last: Vec<u8> = std::iter::repeat(iv_key).take(16).collect();
-    text.as_bytes()
-        .chunks(16)
-        .for_each(|chunk| last = encrypt_aes_block(chunk, key.as_bytes(), &last));
-    last
-}
+#[cfg(test)]
+mod test {
+    use crate::challengetwo::two::pad_to_length;
 
-#[test]
-fn u9_pad_to_length() {
-    assert_eq!(vec![1, 2, 3, 2, 2], pad_to_length(&[1, 2, 3], 5));
-}
+    use super::{decrypt_aes_cbc, encrypt_aes_cbc};
+    #[test]
+    fn test_c10() {
+        let msg = "Some secret message";
+        let key = "YELLOW SUBMARINE";
+        let iv = "\x00".repeat(16);
 
-#[test]
-fn u10_encrypt_aes_block() {
-    let result = encrypt_aes_block_str("hallo", "YELLOW SUBMARINE", "hallo");
-    assert_eq!(
-        result,
-        &[118, 209, 203, 75, 175, 162, 70, 226, 227, 175, 3, 93, 108, 19, 195, 114]
-    )
-}
-#[test]
-fn u10_encrypt_aes() {
-    // println!(
-    //     "from utf{:?}",
-    //     std::str::from_utf8(&[193, 52, 7, 4, 160, 35, 212, 99, 65, 139, 150, 251, 170, 254, 153])
-    //         .unwrap()
-    // );
-    let result = encrypt_with_aes("Hallo wie geht es dir", 10, "YELLOW SUBMARINE");
-    assert_eq!(
-        result,
-        &[193, 52, 7, 4, 160, 35, 239, 212, 99, 65, 139, 150, 251, 170, 254, 153],
-    );
+        let encrypted_msg_hex = encrypt_aes_cbc(msg, key, '\x00' as u8, 16);
+        let decrypted_msg = decrypt_aes_cbc(encrypted_msg_hex.as_str(), key, '\x00' as u8, 16);
+        assert_eq!(msg, decrypted_msg);
+    }
+
+    #[test]
+    fn u9_pad_to_length2() {
+        assert_eq!(vec![1, 2, 3], pad_to_length(&[1, 2, 3], 1));
+        assert_eq!(vec![1, 2, 3], pad_to_length(&[1, 2, 3], 3));
+        assert_eq!(vec![1, 2, 3, 1], pad_to_length(&[1, 2, 3], 2));
+        assert_eq!(vec![1, 2, 3, 1], pad_to_length(&[1, 2, 3], 4));
+        assert_eq!(vec![1, 2, 3, 1], pad_to_length(&[1, 2, 3], 4));
+        assert_eq!(vec![1, 2, 3, 2, 2], pad_to_length(&[1, 2, 3], 5));
+        assert_eq!(vec![1, 2, 3, 4, 2, 2], pad_to_length(&[1, 2, 3, 4], 3));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use crate::challengeone::one::find_encrypted;
 
 mod challengeone;
 pub mod challengetwo;
-
+pub mod utils;
 fn main() {}
